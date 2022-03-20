7297b5dae2daac9058f4c5e48eb4a328e9f088dc
Akash Kamble <akashkamble8656@gmail.com>
2022-03-20 20:51:58 +0530

    Sqldelight (#13)
    * Implementing SQLDelight.

---
+++ app/src/main/java/com/akashk/palette/core/di/DataModule.kt
@@ -0,0 +1,39 @@
+package com.akashk.palette.core.di
+
+import android.content.Context
+import com.akash.palette.sqldelight.PaletteQueries
+import com.akashk.palette.Database
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataModule {
+
+    @Provides
+    @Singleton
+    fun provideSqlDelightDataBase(
+        @ApplicationContext
+        applicationContext: Context
+    ): Database {
+        val driver: SqlDriver = AndroidSqliteDriver(
+            schema = Database.Schema,
+            context = applicationContext,
+            name = "palette.db"
+        )
+        return Database(driver)
+    }
+
+    @Provides
+    fun providePaletteQueries(
+        database: Database
+    ): PaletteQueries {
+        return database.paletteQueries
+    }
+}
---
+++ app/src/main/java/com/akashk/palette/domain/data/LocalPaletteRepository.kt
@@ -0,0 +1,84 @@
+package com.akashk.palette.domain.data
+
+import com.akash.palette.sqldelight.LocalPalette
+import com.akash.palette.sqldelight.PaletteQueries
+import com.akashk.palette.core.Result
+import com.squareup.sqldelight.runtime.coroutines.asFlow
+import com.squareup.sqldelight.runtime.coroutines.mapToList
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.lang.Exception
+import javax.inject.Inject
+
+class LocalPaletteRepository @Inject constructor(
+    private val queries: PaletteQueries
+) : PaletteRepository {
+    override fun fetchAllPalettes(): Flow<Result<List<Palette>>> {
+        return queries.selectAll()
+            .asFlow()
+            .mapToList()
+            .map { paletteList ->
+                val domainPalettes = paletteList.map { localPalette ->
+                    localPalette.toPalette()
+                }
+                Result.Success(domainPalettes)
+            }
+    }
+
+    override suspend fun addPalette(palette: Palette): Result<Unit> {
+        return try {
+            queries
+                .insertPalette(palette.toLocalPalette())
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    override suspend fun deletePalette(palette: Palette): Result<Unit> {
+        return try {
+            queries.deletePalette(palette.id)
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    override suspend fun updatePalette(newPalette: Palette): Result<Unit> {
+        return try {
+            queries.insertPalette(newPalette.toLocalPalette())
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    override fun fetchPaletteById(id: String): Palette {
+        return try {
+            queries
+                .getPaletteById(id)
+                .executeAsOne()
+                .toPalette()
+        } catch (e: Exception) {
+            throw IllegalArgumentException("palette with provided id is not present in the database")
+        }
+    }
+
+    private fun LocalPalette.toPalette(): Palette {
+        return Palette(
+            id = this.id,
+            name = this.name,
+            colorList = this.colors.split(",").toMutableList(),
+            modifiedAt = this.modifiedAt.toLong()
+        )
+    }
+
+    private fun Palette.toLocalPalette(): LocalPalette {
+        return LocalPalette(
+            id = this.id,
+            name = this.name,
+            colors = this.colorList.joinToString(),
+            modifiedAt = this.modifiedAt.toString()
+        )
+    }
+}
